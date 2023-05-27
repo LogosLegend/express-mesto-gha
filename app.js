e7c14868052501247a3ac4e630ce57a485e950a8
LogosLegend <valeracalico@gmail.com>
2023-05-28 01:44:21 +0600

    f

--- app.js
+++ app.js
@@ -6,47 +6,25 @@ const { login, createUser } = require('./controllers/users');
 const { errorHandler } = require('./middlewares/errorHandler')
 const { errors, celebrate, Joi } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('cors')
 
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const allowedCors = [
-  'http://logos.nomoredomains.rocks',
-  'https://logos.nomoredomains.rocks',
-  'http://api.logos.nomoredomains.rocks',
-  'https://api.logos.nomoredomains.rocks',
-  'localhost:3000'
-];
-
-app.use(function(req, res, next) {
-
-  const { origin } = req.headers;
-
-  // res.header('Access-Control-Allow-Origin', "*");
-  // res.header('Access-Control-Allow-Credentials', true);
-
-  if (allowedCors.includes(origin)) {
-
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
-  
-  if (method === 'OPTIONS') {
-
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-  }
-
-  next();
-});
+app.use(cors({
+  origin: [
+  "http://logos.nomoredomains.rocks",
+  "https://logos.nomoredomains.rocks",
+  "http://api.logos.nomoredomains.rocks",
+  "https://api.logos.nomoredomains.rocks",
+  "localhost:3000"
+  ],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  method: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 
 app.use(cookieParser());
 

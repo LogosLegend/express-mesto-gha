58258f1fb6544d8981dd9ee8c3795697e99a2328
LogosLegend <valeracalico@gmail.com>
2023-05-27 18:03:59 +0600

    cors

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ const { errorHandler } = require('./middlewares/errorHandler')
 const { errors, celebrate, Joi } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'http://logos.nomoredomains.rocks/',
+  'https://logos.nomoredomains.rocks/',
+  'localhost:3000'
+];
+
+app.use(function(req, res, next) {
+
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  
+  if (method === 'OPTIONS') {
+
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+  }
+
+  next();
+});
+
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;

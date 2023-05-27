ea9ca3f1ba081fc5b15abe81ba1ba00bcd5982ff
LogosLegend <valeracalico@gmail.com>
2023-05-27 23:44:03 +0600

    f

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(function(req, res, next) {
   if (allowedCors.includes(origin)) {
 
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   const { method } = req;

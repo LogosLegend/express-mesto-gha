b3df9c924dc47f93385f5fb3f38ddfc071b1e0ee
LogosLegend <valeracalico@gmail.com>
2023-05-30 04:41:54 +0600

    fix

--- models/user.js
+++ models/user.js
@@ -9,62 +9,71 @@ const {
 
 } = require('../utils/constants');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
 
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Жак-Ив Кусто',
-  },
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Жак-Ив Кусто',
+    },
 
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Исследователь',
-  },
+    about: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Исследователь',
+    },
 
-  avatar: {
-    type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: {
-      validator: (v) => /https?:\/\/(www\.)?[a-zA-Z0-9-_~:?#[\]@!$&'()*+,;=]{1,}\.[a-zA-Z0-9.\-_~:/?#[\]@!$&'()*+,;=]{1,}/i.test(v),
-      message: 'Неправильный формат ссылки',
+    avatar: {
+      type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: {
+        validator: (v) => /https?:\/\/(www\.)?[a-zA-Z0-9-_~:?#[\]@!$&'()*+,;=]{1,}\.[a-zA-Z0-9.\-_~:/?#[\]@!$&'()*+,;=]{1,}/i.test(v),
+        message: 'Неправильный формат ссылки',
+      },
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Неправильный формат почты',
+      },
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
+  {
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (v) => validator.isEmail(v),
-      message: 'Неправильный формат почты',
+    statics: {
+      findUserByCredentials(email, password) {
+        return this.findOne({ email }).select('+password')
+          .then((user) => {
+            if (!user) {
+              return Promise.reject(new Unauthorized(errorCodeMessage401));
+            }
+
+            return bcrypt.compare(password, user.password)
+              .then((matched) => {
+                if (!matched) {
+                  return Promise.reject(new Unauthorized(errorCodeMessage401));
+                }
+
+                return user;
+              });
+          });
+      },
     },
   },
-
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-}, { toObject: { useProjection: true }, toJSON: { useProjection: true } });
-
-userSchema.statics.findUserByCredentials = ((email, password) => this.findOne({ email }).select('+password')
-  .then((user) => {
-    if (!user) {
-      return Promise.reject(new Unauthorized(errorCodeMessage401));
-    }
-
-    return bcrypt.compare(password, user.password)
-      .then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Unauthorized(errorCodeMessage401));
-        }
-
-        return user;
-      });
-  }));
+  { toObject: { useProjection: true }, toJSON: { useProjection: true } },
+);
 
 module.exports = mongoose.model('user', userSchema);

915562a80618156f97b126c26e6310b9d7ee9e14
LogosLegend <valeracalico@gmail.com>
2023-06-08 06:09:38 +0600

    port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { errorCodeUrlMessage404 } = require('./utils/constants');
 
 const auth = require('./middlewares/auth');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(cors({
